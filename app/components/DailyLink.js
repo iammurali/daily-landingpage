27017c37f1c97a2738c7e97f218a0814fbf955c2
muralielumalai <muralismail4u@gmail.com>
2023-07-23 02:28:31 +0530

    fix graffiti

--- app/components/DailyLink.js
+++ app/components/DailyLink.js
@@ -11,12 +11,12 @@ const DailyLink = () => {
         <section className="bg-black px-8 py-8 md:px-40 md:py-20 text-center lg:h-screen flex justify-center items-center flex-col">
 
             <span className='text-white text-5xl md:text-7xl font-bold inline-block pt-8 relative'>
-                <span className="absolute top-5 -right-52">
+                <span className="absolute -top-5 md:top-5 -right-10 md:-right-52">
 
                     <Graffiti2 />
                 </span>
                 Daily <span className='text-blue-500'>Link.</span>
-                <span className="absolute -bottom-8 -left-64">
+                <span className="absolute bottom-10 md:-bottom-8 -left-12 md:-left-64">
 
                     <Graffiti1 />
                 </span>
